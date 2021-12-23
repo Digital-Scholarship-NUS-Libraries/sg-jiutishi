60c0ed5e3376563cca7cdd43e39bee0e767a7e80
karenlaw-0316 <karen64364897>
2021-12-23 08:22:10 +0800

    update on slideshow

--- src/pages/topicTemplate.js
+++ src/pages/topicTemplate.js
@@ -1,11 +1,19 @@
 import data from "../data/Poem.json"
+import topicData from "../data/Topic.json"
 import React from "react"
 import { topicMain } from "../../style.module.css"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import { Container } from "react-bootstrap"
+import { Container, Carousel, Image } from "react-bootstrap"
 
 export default function topicTemplate({ pageContext: { topic } }) {
+  function getSlideImage() {
+    const getTopic = topicData.filter(data => data.chn_name === topic)
+    const slideImageArray = getTopic[0].slider
+    console.log(slideImageArray)
+    return slideImageArray
+  }
+
   function sortByProperty(property) {
     return function (a, b) {
       if (a[property] > b[property]) return 1
@@ -32,30 +40,69 @@ export default function topicTemplate({ pageContext: { topic } }) {
     return dict
   }
   const sortedData = getSortedNYFT()
+  const slideImage = getSlideImage()
   const final = Object.entries(sortedData)
-  console.log(final)
+
   return (
     <Layout>
       <Seo title="Home" />
+      <Carousel>
+        {slideImage.map((image, i) => (
+          <Carousel.Item key={i}>
+            <Image
+              className="d-block w-100"
+              style={{ width: "1920px", height: "700px", objectFit: "cover" }}
+              src={require(`../images/slider/${image}`).default}
+              alt="First slide"
+            />
+            <Carousel.Caption style={{ left: "30%", right: "30%" }}>
+              <div
+                style={{
+                  backgroundColor: "rgba(0,0,0,0.5)",
+                  display: "flex",
+                  padding: "10px",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <button
+                  class="btn btn-outline-light rounded-0"
+                  type="button"
+                  style={{ margin: "5px", borderRadius: "0px !important" }}
+                >
+                  濱海灣
+                </button>
+                <br />
+                <h4>雙林寺</h4>
+                <br />
+                <p></p>
+              </div>
+            </Carousel.Caption>
+          </Carousel.Item>
+        ))}
+      </Carousel>
       <Container>
-        <div className={topicMain}>
-          {final.map((author, i) => (
-            <div style={{ margin: "30px" }}>
-              <h4 style={{ marginBottom: "20px" }}>{author[0]}</h4>
+        <div>
+          <div className={topicMain}>
+            {final.map((author, i) => (
+              <div style={{ margin: "30px" }}>
+                <h4 style={{ marginBottom: "20px" }}>{author[0]}</h4>
 
-              {author[1].map(poem => (
-                <a href={`/poem/${poem.author_name}/${poem.title}`}>
-                  <button
-                    class="btn btn-outline-dark rounded-0"
-                    type="button"
-                    style={{ margin: "5px", borderRadius: "0px !important" }}
-                  >
-                    {poem.title}
-                  </button>
-                </a>
-              ))}
-            </div>
-          ))}
+                {author[1].map(poem => (
+                  <a href={`/poem/${poem.author_name}/${poem.title}`}>
+                    <button
+                      class="btn btn-outline-dark rounded-0"
+                      type="button"
+                      style={{ margin: "5px", borderRadius: "0px !important" }}
+                    >
+                      {poem.title}
+                    </button>
+                  </a>
+                ))}
+              </div>
+            ))}
+          </div>
         </div>
       </Container>
     </Layout>

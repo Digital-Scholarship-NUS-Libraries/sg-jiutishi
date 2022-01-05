7c409cb8457c9347fb77c35adb9d7c011e3c0867
karenlaw-0316 <karen64364897>
2022-01-06 03:16:28 +0800

    update on images

---
+++ src/components/Content/新洲雅苑懷舊集AuthorDetails.js
@@ -0,0 +1,27 @@
+import React from "react"
+import {
+  poemDiv,
+  poemAuthorTitle,
+  poemContent,
+  poemMain,
+  logoImage,
+} from "../../../style.module.css"
+import authors from "../../data/Poet.json"
+
+export default function AuthorDetails(poet) {
+  function getAuthor() {
+    const filtered = authors.filter(author => author.fullName === poet.poet)
+    return filtered[0].description
+  }
+
+  const foundAuthor = getAuthor()
+
+  return (
+    <div
+      className={poemDiv}
+      style={{ marginLeft: "0px", marginTop: "50px", marginBottom: "50px" }}
+    >
+      <p className={poemContent}>{foundAuthor}</p>
+    </div>
+  )
+}
